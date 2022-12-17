11d52b350ee48f9c0be73da44a5da0d63ad6f2b4
Salih Ahmet <salihahmet.nuhoglu@gmail.com>
2022-12-17 20:24:38 +0300

    last commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '37fcc25c95mshd46a5e790e7c5a4p1e1b82jsne88b97b41a3b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "0e11041ec2b8b96ff637ad17910ad6c0";
